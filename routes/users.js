98e6f37c9a1a801607f24ef274dd153839d4d30b
Akshat <akshatsaraswat03@gmail.com>
2020-10-25 16:41:16 +0530

    made the first model

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,24 @@
 const express = require('express')
 const router = express.Router()
+const { check, validationResult } = require('express-validator')
 
-router.post('/', (req,res)=> {
-  res.send('register a user')
+const User = require('../models/User')
+
+router.post('/', [
+  check('name', 'Name is needed')
+    .not().isEmpty(),
+  check('email', 'Please enter email')
+    .isEmail(),
+  check('password', 'Please enter pass with 6 or more characters')
+    .isLength({min: 6})
+] ,
+(req,res)=> {
+  const errors = validationResult(req)
+  if(!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() })
+  }
+
+    res.send("passed")
 })
 
 module.exports = router
--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 const express = require('express')
+const connectDB = require('./config/db')
 
 const app = express()
-const PORT = process.env.PORT || 6000
+
+//connection with db made
+connectDB()
 
 app.use('/api/users', require('./routes/users'))
 app.use('/api/auth', require('./routes/auth'))
 app.use('/api/contacts', require('./routes/contacts'))
+
+//middleware for post requests
+app.use(express.json({extended: false}))
+
 
 app.get('/', (req, res)=> {
   res.json({msg: 'contact app'})
@@ -14,7 +21,7 @@ app.get('/', (req, res)=> {
 
 
 
-
+const PORT = process.env.PORT || 6000
 
 app.listen(PORT, ()=> {
   console.log(`server started on port ${PORT}`)
