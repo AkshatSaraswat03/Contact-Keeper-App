db76b908230c030b023cd4ae3270b6fcfe41ebf2
Akshat <akshatsaraswat03@gmail.com>
2020-10-27 17:54:08 +0530

    frontend, navbar

---
+++ client/src/components/pages/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+      <h1> About this app </h1>
+      <p className="my-1">
+        This is a MERN Stack react app for keeping your contacts
+      </p>
+    </div>
+  )
+}
+
+export default About
