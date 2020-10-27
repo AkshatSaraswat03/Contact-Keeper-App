36a7f0380295e87a8cbd94f001701e2794b14263
Akshat <akshatsaraswat03@gmail.com>
2020-10-27 06:51:07 +0530

    add contacts

---
+++ models/Contact.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose')
+
+const ContactSchema = new mongoose.Schema({ 
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'users'
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  email:{
+    type : String,
+    required: true,
+  },
+  phone:{
+    type : String
+  },
+  type:{
+    type: String, 
+    default: 'personal'
+  },
+  date:{
+    type : Date,
+    default: Date.now
+  } 
+});
+
+const model = mongoose.model('contact', ContactSchema);
+module.exports = model
--- routes/contacts.js
+++ routes/contacts.js
@@ -1,19 +1,61 @@
 const express = require('express')
 const router = express.Router()
+const { check, validationResult } = require('express-validator')
+const User = require('../models/User')
+const Contact = require('../models/Contact')
+const auth = require('../middlewares/auth')
 
-router.get('/', (req,res)=> {
-  res.send('get all contacts')
+//get all contacts of user
+router.get('/', auth, async (req,res)=> {
+
+  try {
+    const contacts = await Contact.find({user: req.user.id}).sort({ date: -1})
+    res.json(contacts)
+
+  } catch (err) {
+    console.error(err.message)
+    res.status(500).send("server error")
+  }
 })
 
-router.post('/', (req,res)=> {
-  res.send('add contact')
+//add a contact
+router.post('/', [auth, [
+  check('name', 'Name is required').not().isEmpty()
+] ], 
+async (req,res)=> {
+  
+  const errors = validationResult(req)
+  if(!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() })
+  }
+
+  const {name, email, phone, type} = req.body
+
+  try {
+    const newContact = new Contact({
+      name, 
+      email,
+      phone,
+      type,
+      user: req.user.id
+    })
+
+    const contact = await newContact.save()
+    res.json(contact)
+
+  } catch (err) {
+    console.error(err.message)
+    res.status(500).send('server error')
+    
+  }
+
 })
 
-router.put('/:id', (req,res)=> {
+router.put('/:id', auth, (req,res)=> {
   res.send('update contact')
 })
 
-router.delete('/:id', (req,res)=> {
+router.delete('/:id', auth, (req,res)=> {
   res.send('delete contact')
 })
 
